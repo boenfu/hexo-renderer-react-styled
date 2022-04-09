6247c197510191f3d4fb2bd2d25aa098697da29a
boenfu <1997@boenfu.cn>
2022-04-09 23:03:14 +0800

    support import components

--- src/library/@renderer.ts
+++ src/library/@renderer.ts
@@ -1,16 +1,17 @@
-import vm from 'vm';
+import * as FS from 'fs';
 
 import {createElement} from 'react';
 import {renderToStaticMarkup} from 'react-dom/server';
 import {ServerStyleSheet, StyleSheetManager} from 'styled-components';
 import {transform} from 'sucrase';
+import {NodeVM, NodeVMOptions} from 'vm2';
 
 import {ComponentProps} from './props';
 
 export async function renderer(
   {
     text,
-    path,
+    path: filePath,
   }: {
     text: string;
     path?: string;
@@ -20,26 +21,47 @@ export async function renderer(
   let sheet = new ServerStyleSheet();
 
   try {
-    let js = transform(text, {
-      transforms: ['typescript', 'imports', 'jsx'],
-      production: true,
-      filePath: path,
-    }).code;
-
-    let context: vm.Context = {
-      require,
-      console,
-      exports: {},
+    let compiler = (text: string, file: string): string => {
+      return transform(text, {
+        transforms: ['typescript', 'imports', 'jsx'],
+        production: true,
+        enableLegacyBabel5ModuleInterop: true,
+        filePath: file,
+      }).code;
     };
 
-    vm.runInNewContext(js, context, {
-      displayErrors: true,
-    });
+    let options: NodeVMOptions = {
+      console: 'inherit',
+      eval: false,
+      sourceExtensions: ['js', 'jsx', 'ts', 'tsx'],
+      compiler(code, file) {
+        if (!file.endsWith('js')) {
+          return compiler(code, file);
+        }
+
+        return code;
+      },
+      require: {
+        external: true,
+        customRequire(id: string) {
+          if (!id.includes('node_modules')) {
+            return NodeVM.code(
+              compiler(FS.readFileSync(id, 'utf8'), id),
+              id,
+              options,
+            );
+          }
+
+          // eslint-disable-next-line @typescript-eslint/no-require-imports
+          return require(id);
+        },
+      },
+    };
 
     let html = renderToStaticMarkup(
       createElement(StyleSheetManager, {
         sheet: sheet.instance,
-        children: createElement(context.exports.default, props),
+        children: createElement(NodeVM.code(text, filePath, options), props),
       }),
     );
 
