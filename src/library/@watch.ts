27f37d844338b16aadebfc067d8cee10711b4987
boenfu <1997@boenfu.cn>
2022-04-13 20:28:04 +0800

    support cache file starts with _

--- src/library/@watch.ts
+++ src/library/@watch.ts
@@ -1,22 +1,16 @@
+import * as Path from 'path';
+
 import decache from 'decache';
 import {defaultContentCompiler} from 'require-tsx';
-import hash from 'string-hash';
-
-const moduleHashMap = new Map<string, number>();
 
 export function watchComplier(
   this: any,
   content: string,
   fileName: string,
 ): string {
-  let existed = moduleHashMap.get(fileName);
-  let current = hash(content);
-
-  if (existed && existed !== current) {
+  if (!Path.basename(fileName).startsWith('_')) {
     decache(fileName);
   }
-
-  moduleHashMap.set(fileName, current);
 
   return defaultContentCompiler.call(this, content, fileName);
 }
